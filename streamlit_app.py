2ef64c5fcba9ee0e9e9631ff585f4e20d9f66c7b
Elliot L <150163724+shadowoframmerd@users.noreply.github.com>
2026-06-23 08:56:46 +0100

    Import Streamlit package in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Import python packages
-from snowflake.snowpark.context import get_active_session
+import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
